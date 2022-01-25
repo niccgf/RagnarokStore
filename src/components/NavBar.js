6d1813cd3b6facb8ee02e9cede27b391e64d5e35
nicfalcon <84859424+niccgf@users.noreply.github.com>
2022-01-25 11:49:46 -0300

    Desafío 1: Corrección ícono del sitio

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,4 @@
+import icono from './icono.svg'
 
 // En estos dos let, defino los atributos de mi ícono de carrito de compras
 
@@ -12,6 +13,11 @@ let atributosIconoCarrito = {
 
 let pathIconoCarrito = {
     d: "M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1v4.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 13.5V9a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h1.217L5.07 1.243a.5.5 0 0 1 .686-.172zM2 9v4.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V9H2zM1 7v1h14V7H1zm3 3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 4 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 6 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 8 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5z"
+}
+
+let styleIcono = {
+  width:"50px",
+  height:"50px"
 }
 
 // Declaro la función IconoCarrito unicamente para el ícono
@@ -33,7 +39,7 @@ function NavBar() {
         <div className="container">
           <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
             <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                <img src="../components/icono.svg" alt="icono"/>
+                <img src={icono} alt="icono" style={styleIcono}/>
             </a>
             <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
               <li><a href="#" className="nav-link px-2 selected">Inicio</a></li>
